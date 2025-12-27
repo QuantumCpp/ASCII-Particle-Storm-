6bc31fec37afc2c7218bb1544e4173ae6c418b7b
QuantumCpp <jeremynobel16@gmail.com>
2025-12-26 21:36:46 -0500

    Refactor rain mode: Actualizacion del modo en como se dibujan los rectangulos

--- rain-mode/rain-ascii.h
+++ rain-mode/rain-ascii.h
@@ -6,20 +6,9 @@
 class RainAscii{
   private:
     Vector2 dimension = {5.0, 5.0};
-    Vector2 Posicion = {(float(WidthScreen)/2), (float(HeigthScreen)/2)};
-    
-    
-
+    Vector2 Posicion = {(float(WidthScreen)/2), (float(HeigthScreen)/2)};   
   public:
     void CreateObject(){
-      //Linea vertical derecha
-      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y - dimension.y), int(Posicion.x - dimension.x), int(Posicion.y + dimension.y), WHITE);
-      //Linea vertical izquierda
-      DrawLine(int(Posicion.x + dimension.x), int(Posicion.y - dimension.y), int(Posicion.x + dimension.x), int(Posicion.y + dimension.y), WHITE);
-      //Linea horizontal inferior
-      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y + dimension.y), int(Posicion.x + dimension.x), int(Posicion.y + dimension.y), WHITE);
-      //Linea horizontal superior
-      DrawLine(int(Posicion.x - dimension.x), int(Posicion.y - dimension.y), int(Posicion.x + dimension.x), int(Posicion.y - dimension.y), WHITE);
+      DrawRectangle(Posicion.x, Posicion.y, int(dimension.x), int(dimension.y), WHITE);
     }
-
 };
